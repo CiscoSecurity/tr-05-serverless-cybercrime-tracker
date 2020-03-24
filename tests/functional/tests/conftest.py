d47e221322c2f33ec8bcce982bf8be8db251bf04
Denys Kulykov <dkuly@softserveinc.com>
2020-03-24 14:13:28 +0200

    add travis key

--- tests/functional/tests/conftest.py
+++ tests/functional/tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 
 from ctrlibrary.core import settings
 from ctrlibrary.threatresponse import token
-from ctrlibrary.relay_api.base import RelayApiToken, RelayApi
 
 
 def pytest_collection_modifyitems():
@@ -22,26 +21,3 @@ def module_token():
 @pytest.fixture(scope='module')
 def module_headers(module_token):
     return {'Authorization': 'Bearer {}'.format(module_token)}
-
-
-@pytest.fixture(scope='session')
-def session_headers():
-    return {'Authorization': 'Bearer {}'.format(
-        settings.server.app_client_password)}
-
-
-@pytest.fixture(scope='session')
-def relay_api(session_headers):
-    return RelayApiToken(
-        hostname=settings.server.app_hostname,
-        prefix='',
-        token={'headers': session_headers}
-    )
-
-
-@pytest.fixture(scope='session')
-def relay_api_without_token(session_headers):
-    return RelayApi(
-        hostname=settings.server.app_hostname,
-        prefix=''
-    )
