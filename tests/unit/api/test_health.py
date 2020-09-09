e9685f8ccd0b68cdaf4cc0fa531e12a626041012
skhare <skhare@softserveinc.com>
2020-09-09 17:58:04 +0300

    CCTRI-1566: Add gitter badge to README.md and Add unittest for health

---
+++ tests/unit/api/test_health.py
@@ -0,0 +1,33 @@
+from http import HTTPStatus
+from pytest import fixture
+from unittest import mock
+
+def routes():
+    yield '/health'
+
+
+@fixture(scope='module', params=routes(), ids=lambda route: f'POST {route}')
+def route(request):
+    return request.param
+
+
+def test_health_call_success(route, client):
+    response = client.post(route)
+    assert response.status_code == HTTPStatus.OK
+
+
+@mock.patch('requests.get')
+def test_health_call_error(response_mock, client):
+    exp_response = {'errors': [
+        {
+            'code': 'invalid_health_check',
+            'message': 'Invalid 3rd party API connect.',
+        },
+    ]}
+
+    response_mock.return_value.ok = False
+
+    response = client.post('/health')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == exp_response
