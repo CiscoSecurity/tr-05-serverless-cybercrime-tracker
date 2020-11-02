6281d1018e05f2040c6363de260d3177e5c23444
MaxMhonc <71133140+MaxMhonc@users.noreply.github.com>
2020-11-02 11:14:08 +0200

    Implement version endpoint (#19)

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, jsonify
 from api.enrich import enrich_api
 from api.health import health_api
 from api.respond import respond_api
+from api.version import version_api
 from api.utils import jsonify_errors
 from api.errors import TRError
+
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ app.config.from_object('config.Config')
 app.register_blueprint(health_api)
 app.register_blueprint(enrich_api)
 app.register_blueprint(respond_api)
+app.register_blueprint(version_api)
 
 
 @app.errorhandler(Exception)
