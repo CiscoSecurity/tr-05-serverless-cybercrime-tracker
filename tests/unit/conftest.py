73c223f8bd9ab5c5622f6987f6515c76d2e6e854
anastasiia.rozlyvan <nastia.2.r.2@gmail.com>
2021-02-15 16:41:24 +0200

    Delete unused secret key fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,18 +1,10 @@
-from datetime import datetime
-
 from pytest import fixture
 
 from app import app
 
 
 @fixture(scope='session')
-def secret_key():
-    # Generate some string based on the current datetime.
-    return datetime.utcnow().isoformat()
-
-
-@fixture(scope='session')
-def client(secret_key):
+def client():
     app.testing = True
 
     with app.test_client() as client:
